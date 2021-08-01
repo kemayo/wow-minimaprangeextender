068e14b034440ab9db878f515965c64fa29f5198
David Lynch <kemayo@gmail.com>
2021-08-01 13:29:18 -0500

    Sync up with SilverDragon_RangeExtender, adding mystery vignettes

--- addon.lua
+++ addon.lua
@@ -1,4 +1,5 @@
 local myname, ns = ...
+local module = ns
 
 local HBD = LibStub("HereBeDragons-2.0")
 local HBDPins = LibStub("HereBeDragons-Pins-2.0")
@@ -10,8 +11,43 @@ end)
 f:RegisterEvent("ADDON_LOADED")
 f:Hide()
 
+local db
+local compat_disabled = false
+
+local function setDefaults(options, defaults)
+	setmetatable(options, { __index = function(t, k)
+		if type(defaults[k]) == "table" then
+			t[k] = setDefaults({}, defaults[k])
+			return t[k]
+		end
+		return defaults[k]
+	end, })
+	-- and add defaults to existing tables
+	for k, v in pairs(options) do
+		if defaults[k] and type(v) == "table" then
+			setDefaults(v, defaults[k])
+		end
+	end
+	return options
+end
+
 function ns:ADDON_LOADED(event, name)
 	if name ~= myname then return end
+
+	_G[myname.."DB"] = setDefaults(_G[myname.."DB"] or {}, {
+		enabled = true,
+		mystery = true,
+		types = {
+			vignettekill = true,
+			vignettekillelite = true,
+			vignetteloot = true,
+			vignettelootelite = true,
+			vignetteevent = true,
+			vignetteeventelite = true,
+		},
+	})
+	db = _G[myname.."DB"]
+	ns.db = db
 
 	self.pool = CreateFramePool("FRAME", Minimap, "MinimapRangeExtenderPinTemplate")
 
@@ -19,15 +55,20 @@ function ns:ADDON_LOADED(event, name)
 	f:RegisterEvent("VIGNETTES_UPDATED")
 
 	self:VIGNETTES_UPDATED()
+
+	f:UnregisterEvent("ADDON_LOADED")
 end
 
+-- Everything below here is just kept in sync with SilverDragon_RangeExtender
 
 local vignetteIcons = {
 	-- [instanceid] = icon
 }
 
-function ns:VIGNETTE_MINIMAP_UPDATED(event, instanceid, onMinimap, ...)
+function module:VIGNETTE_MINIMAP_UPDATED(event, instanceid, onMinimap, ...)
+	-- Debug("VIGNETTE_MINIMAP_UPDATED", instanceid, onMinimap, ...)
 	if not instanceid then
+		-- ...just in case
 		return
 	end
 
@@ -42,11 +83,12 @@ function ns:VIGNETTE_MINIMAP_UPDATED(event, instanceid, onMinimap, ...)
 		icon.texture:Show()
 	end
 end
-function ns:VIGNETTES_UPDATED()
+function module:VIGNETTES_UPDATED()
 	local vignetteids = C_VignetteInfo.GetVignettes()
+	-- Debug("VIGNETTES_UPDATED", #vignetteids)
 
 	for instanceid, icon in pairs(vignetteIcons) do
-		if not tContains(vignetteids, instanceid) then
+		if not tContains(vignetteids, instanceid) or (icon.info and not db.types[icon.info.atlasName:lower()]) or (not icon.info and not db.mystery) or not db.enabled then
 			HBDPins:RemoveMinimapIcon(self, icon)
 			icon:Hide()
 			icon.info = nil
@@ -60,35 +102,45 @@ function ns:VIGNETTES_UPDATED()
 	end
 end
 
-function ns:UpdateVignetteOnMinimap(instanceid)
+function module:UpdateVignetteOnMinimap(instanceid)
+	if compat_disabled or not db.enabled then
+		return
+	end
+	-- Debug("considering vignette", instanceid)
 	local uiMapID = HBD:GetPlayerZone()
 	if not uiMapID then
-		return
+		return -- Debug("can't determine current zone")
 	end
 	local vignetteInfo = C_VignetteInfo.GetVignetteInfo(instanceid)
-	if not (vignetteInfo and vignetteInfo.vignetteGUID and vignetteInfo.atlasName) then
-		return
+	if not db.mystery and not (vignetteInfo and vignetteInfo.vignetteGUID and vignetteInfo.atlasName) then
+		return -- Debug("vignette had no info")
 	end
-	if vignetteInfo.type ~= Enum.VignetteType.Normal then
-		return
+	if vignetteInfo then
+		if vignetteInfo.type ~= Enum.VignetteType.Normal then
+			return -- Debug("vignette isn't normal")
+		end
+		if not db.types[vignetteInfo.atlasName:lower()] then
+			return -- Debug("vignette type not enabled", vignetteInfo.atlasName)
+		end
 	end
-	local position = C_VignetteInfo.GetVignettePosition(vignetteInfo.vignetteGUID, uiMapID)
+	local position = C_VignetteInfo.GetVignettePosition(instanceid, uiMapID)
 	if not position then
-		return
+		return -- Debug("vignette had no position")
 	end
 	local x, y = position:GetXY()
 
 	local icon = vignetteIcons[instanceid]
 	if not icon then
 		icon = self.pool:Acquire()
-		icon.texture:SetAtlas(vignetteInfo.atlasName)
+		icon.texture:SetAtlas(vignetteInfo and vignetteInfo.atlasName or "poi-nzothvision")
 		icon.texture:SetDesaturated(true)
 		vignetteIcons[instanceid] = icon
 		HBDPins:AddMinimapIconMap(self, icon, uiMapID, x, y, false, true)
+		-- icon.instanceid = instanceid
 		icon.info = vignetteInfo
 	end
 
-	if vignetteInfo.onMinimap then
+	if vignetteInfo and vignetteInfo.onMinimap then
 		icon.texture:Hide()
 	else
 		icon.texture:Show()
@@ -97,13 +149,13 @@ function ns:UpdateVignetteOnMinimap(instanceid)
 	self:UpdateEdge(icon)
 end
 
-function ns:UpdateEdge(icon)
+function module:UpdateEdge(icon)
 	icon:SetAlpha(HBDPins:IsMinimapIconOnEdge(icon) and 0.6 or 1)
 end
 
 C_Timer.NewTicker(1, function(...)
 	for instanceid, icon in pairs(vignetteIcons) do
-		ns:UpdateEdge(icon)
+		module:UpdateEdge(icon)
 	end
 end)
 
@@ -111,13 +163,18 @@ MinimapRangeExtenderPinMixin = {}
 function MinimapRangeExtenderPinMixin:OnLoad() end
 function MinimapRangeExtenderPinMixin:OnMouseEnter()
 	-- TODO: see VignettePinMixin for PVP bounty vignettes if I want to handle this?
-	if not (self.info and self.info.name) then return end
+	-- Debug("OnMouseEnter", self, self.info and self.info.name)
+	-- if not (self.info and self.info.name) then return end
 	if self:GetCenter() > UIParent:GetCenter() then
 		GameTooltip:SetOwner(self, "ANCHOR_LEFT")
 	else
 		GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
 	end
-	GameTooltip_SetTitle(GameTooltip, self.info.name)
+	GameTooltip_SetTitle(GameTooltip, self.info and self.info.name or UNKNOWN)
+	if not self.info then
+		GameTooltip:AddLine("This mystery vignette has no information available", 1, 1, 1, true)
+	end
+	GameTooltip:Show()
 end
 function MinimapRangeExtenderPinMixin:OnMouseLeave()
 	GameTooltip:Hide()
